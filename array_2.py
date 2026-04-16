fb1ea2ea97d918b26561b65c8f82dfaf34438184
smit <psmit1632@gmail.com>
2026-04-16 17:09:14 +0530

    array_2-1

--- array_2.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def shuffle(self, nums: list[int], n: int) -> list[int]:
-        ans = []
-        for i in range(len(nums) // (len(nums)//n)):
-            ans.append(nums[i])
-            ans.append(nums[i + n])
-        return ans
